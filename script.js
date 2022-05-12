cf486c1e083fbff197618352dcb8265578e333bd
Tamajin <wild.jlq@gmail.com>
2022-05-12 18:03:47 +0200

    début de JS

---
+++ script.js
@@ -0,0 +1,7 @@
+// Je récupère la classe commune de mes deux boutons de submit et je stocke l'information dans une classe.
+const btnSubmit = document.querySelector(".submit-btn");
+
+// Je créé un évènement au click sur le bouton avec une fonction
+
+btnSubmit.addEventListener(onclick) => 
+    alert = "Votre message a bien été reçu, mais ne partez pas si vite ! Si vous ne l'avez pas encore trouvé, il y a un bonus caché quelque part."
