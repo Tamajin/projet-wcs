b2324df8cb7e584ab6d1d44f8269ed64d7082b11
Tamajin <wild.jlq@gmail.com>
2022-05-17 14:34:18 +0200

    bouton CTA relié à la page catégories + tentative avortée de changer son texte avec innerHtml

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+// Changement du texte en cliquant sur mon CTA button
+// Je recupère mon bouton et le stocke dans une variable
+// je créé la fonction associée au futur évènement
+// je définis avec un écouteur d'évènements une action au click
+// je modifie le texte dans le bouton avec un innerHtml
+
+const ctaBtn = document.getElementsByClassName("cta");
+console.log(ctaBtn);
+ctaBtn.onclick = function() {
+    document.innerHTML = "Vous êtes dans la Matrice";
+};
+
 // Je récupère la classe commune de mes deux boutons de submit et je stocke l'information dans une classe.
-const btnSubmit = document.querySelector(".submit-btn");
+// const btnSubmit = document.querySelector(".submit-btn");
 
 // Je créé un évènement au click sur le bouton avec une fonction
 
-btnSubmit.addEventListener(onclick) => 
-    alert = "Votre message a bien été reçu, mais ne partez pas si vite ! Si vous ne l'avez pas encore trouvé, il y a un bonus caché quelque part."
+// btnSubmit.addEventListener("click") => 
+//     alert = "Votre message a bien été reçu, mais ne partez pas si vite ! Si vous ne l'avez pas encore trouvé, il y a un bonus caché quelque part."
  
 
